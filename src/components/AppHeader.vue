82dc6ebc98998048dddad2ab802fabd90584b98b
Azderica <mh97888@gmail.com>
2020-12-24 19:44:34 +0900

    start page

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <button>list-menu</button>
+    <h1> Vote Servie for you</h1>
+    <button v-on:click="getUserDetail"> {{userName}} </button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['userName'],
+    methods: {
+        getUserDetail: function(userName) {
+            this.$emit('loadUserDetail')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
